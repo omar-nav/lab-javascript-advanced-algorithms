fc8faf9f883cd6545f70c8b6b6a88d49eee0dd35
omar-nav <omar.navarro@g.austincc.edu>
2018-08-29 19:00:32 -0500

    create functions for pop and push into array

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,35 @@
-function StackDataStructure () {
+class StackDataStructure {
+  constructor() {
+    this.stackControl = [];
+    this.MAX_SIZE = 8;
+  }
+  isEmpty() {
+    if (this.stackControl.length > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  canPush() {
+    if (this.stackControl.length < this.MAX_SIZE) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  push(a) {
+    if (this.canPush()) {
+      this.stackControl.push(a);
+      return this.stackControl;
+    } else {
+      return "Stack Overflow";
+    }
+  }
+  pop() {
+    if (this.isEmpty()) {
+      return "Stack Underflow";
+    } else {
+      return this.stackControl.pop();
+    }
+  }
 }
